f01db1c84f1f04dda6a51d804f455538dc83f31c
notzith <mkvishvajith@gmail.com>
2023-03-15 11:37:29 +0530

    2

--- builder/views.py
+++ builder/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render, HttpResponse, render, redirect
 from django.http import JsonResponse
 
 
-
-
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
+
 def page_creator(request):
-    Func = open("mysite/templates/custom/header.html","w")
+    Func = open("mysite/templates/custom/header.html", "w")
     Func.write("<h1>Hello world sample</h1>")
     Func.close()
-    return JsonResponse({'status' : 'Success'}, status=200, safe=False)   
+    return JsonResponse({'status': 'Success'}, status=200, safe=False)
